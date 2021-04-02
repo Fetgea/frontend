163315c23be8d5dc1d15f43d281d6adb90d02af7
Dinar <dinar@reaspekt.com>
2021-04-02 14:47:34 +0300

    Почти доделанная верстка

---
+++ js/scipt.js
@@ -0,0 +1,17 @@
+function showSlides(number) {
+    let i;
+    let slides = document.getElementsByClassName("carousel-item");
+    let dots = document.getElementsByClassName("slider-dot");
+    slideIndex = number;
+    if (number > slides.length) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].className = slides[i].className.replace(" active", "");
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].className += " active";
+    dots[slideIndex-1].className += " active";
+}
